160c46247579a769122c1946f82f0a65686eff21
Guransh Deol <91736425+guranshdeol@users.noreply.github.com>
2023-10-20 23:39:35 +0530

    Day 14

---
+++ Day 45/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        while(a < 100){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
