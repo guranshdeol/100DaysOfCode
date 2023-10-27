297a39f3a3983847847e0a2ccc4eb19191912a55
Guransh Deol <91736425+guranshdeol@users.noreply.github.com>
2023-10-27 21:53:35 +0530

    Day 52

---
+++ Day 52/main.js
@@ -0,0 +1,16 @@
+// Define a variable called "name" and assign it a string value
+var name = "John";
+
+// Define a variable called "age" and assign it a number value
+var age = 25;
+
+// Define a function called "greet" that takes a name parameter and logs a greeting message to the console
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Call the greet function and pass in the "name" variable as an argument
+greet(name);
+
+// Log the "age" variable to the console
+console.log(age);
