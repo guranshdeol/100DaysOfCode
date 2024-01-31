2f1dbd8c53b0ff08d6ac8cff650565b8536fd2e8
Guransh Deol <guransh1896.be21@chitkara.edu.in>
2024-01-31 23:03:07 +0530

    Day 31

---
+++ Day 31/main.py
@@ -0,0 +1,14 @@
+name = "Guransh Deol"
+age = 20
+price = 25.99
+old = False
+a = None
+print("My name is:",name)
+print(age, price)  
+print(type(name))
+print(type(age))
+print(type(price))
+print(age==20)
+
+print(type(old))
+print(type(a ))
