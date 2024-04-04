524355962e479e0d7c3ec0e805d27337288cda7c
Guransh Deol <guransh1896.be21@chitkara.edu.in>
2024-04-04 21:56:36 +0530

    Day 95

---
+++ Day 95/Revision.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public  class Revision{
+    public static void main (String args[]){
+        // for (int a = 1 ; a <= 4; a++){
+        //     System.out.println("* * * *");
+        // }
+
+    //     int rev = 0;
+
+    //     for (int n = 10899; n > 0; n/=10){
+    //         int r = n % 10;
+            
+    //         rev = ( rev *10)+ r;
+            
+    //     }
+    //     System.out.println(rev);
+// int i = 1;
+//     do {
+//         System.out.println(i);        
+//         i++;
+//         if(i==4){
+//             break;
+//         }
+//     }
+//     while(i<=5);
+//     System.out.println("i am out of the loop");
+
+        Scanner sc = new Scanner(System.in);
+        
+
+        // while (true){
+        //     int a = sc.nextInt();
+        //     if(a % 10 == 0){
+        //         continue;
+        //     }
+        //     System.out.println(a);
+
+        // }
+        
+
+        int n = sc.nextInt();
+
+        while
+
+    }
+}
