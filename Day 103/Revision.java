4f1ce8feac98ebd8cefe65b4a54e75bb0d5ad05e
Guransh Deol <guransh1896.be21@chitkara.edu.in>
2024-04-12 17:00:58 +0530

    Day 103

---
+++ Day 103/Revision.java
@@ -0,0 +1,10 @@
+public class Revision{
+    public static void main (String args []){
+        for (int line = 1; line <=4; line++){
+            for (int star = 1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
